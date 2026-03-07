7f60bd960a1f10f2861b4cce39f77ad2f875edb1
Matthew Jordaan <matthew.jordaan@sanlam.co.za>
2026-03-07 20:24:47 +0200

    pg migrate

--- .next/server/app/api/auth/[...nextauth]/route.js
+++
@@ -1,9 +0,0 @@
-var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/auth/[...nextauth]/route.js")
-R.c("server/chunks/node_modules_next_19811a6f._.js")
-R.c("server/chunks/node_modules_@auth_core_36df0127._.js")
-R.c("server/chunks/node_modules_jose_dist_webapi_868b124b._.js")
-R.c("server/chunks/node_modules_d31be132._.js")
-R.c("server/chunks/[root-of-the-server]__d62ed5cf._.js")
-R.m("[project]/.next-internal/server/app/api/auth/[...nextauth]/route/actions.js [app-rsc] (server actions loader, ecmascript)")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/auth/[...nextauth]/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/auth/[...nextauth]/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
--- .next/server/app/manifest.webmanifest/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../chunks/[turbopack]_runtime.js")("server/app/manifest.webmanifest/route.js")
-R.c("server/chunks/node_modules_next_c2da1398._.js")
-R.c("server/chunks/[root-of-the-server]__cf17957d._.js")
-R.m("[project]/.next-internal/server/app/manifest.webmanifest/route/actions.js [app-rsc] (server actions loader, ecmascript)")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/manifest--route-entry.js [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/manifest--route-entry.js [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
